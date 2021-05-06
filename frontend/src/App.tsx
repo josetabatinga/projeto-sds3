8ab5bd1cab88995a165fa038ab344fe6df5ef071
Jose Tabatinga <zedetaba@gmail.com>
2021-05-06 00:03:26 -0300

    Basic Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
-function App() {
+import DataTable from "components/Datatable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+const App = () => {
   return (
-    <div>
-      <h1 className = "text-primary">Olá Tabatinga!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+
+        <h1 className="text-primary">Olá Tabatinga!</h1>
+      </div>
+      <DataTable />
+      <Footer />
+    </>
   );
 }
 
